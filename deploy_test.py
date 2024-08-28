fb435271800aa4c6d2a8b152b8e1cb3824a35568
hklee <hklee@example.com>
2024-08-28 14:16:10 +0900

    .

--- deploy_test.py
+++ deploy_test.py
@@ -5,7 +5,7 @@ import matplotlib.font_manager as fm
 
 # 한글 폰트 설정
 # font_path = 'C:\\Windows\\Fonts\\malgun.ttf'  # 필요한 폰트 파일 경로
-font_path = 'malgun.ttf'  # 필요한 폰트 파일 경로
+font_path = 'customFonts/malgun.ttf'  # 필요한 폰트 파일 경로
 font_name = fm.FontProperties(fname=font_path).get_name()
 plt.rc('font', family=font_name)
 
